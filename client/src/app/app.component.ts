257168c69c696f0c3e10bcc4f46667c460824450
shamito <shamito@google.com>
2023-11-16 12:47:35 -0600

    Followed Geeks for Geeks tutorial

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './api.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'client';
+
+export class AppComponent implements OnInit { 
+  title = 'frontEnd'; 
+  message: any; 
+  constructor(private apiService: ApiService) { }; 
+  ngOnInit() { 
+      this.apiService.getMessage().subscribe(data => { 
+          this.message = data; 
+      }); 
+  } 
 }
