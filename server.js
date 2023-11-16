1d875416b345c46b16ca067e049101c24b5542fd
shamito <shamito@google.com>
2023-11-16 15:29:36 -0600

    removing superfulous roles

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Role = db.role;
 
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Db');
-  initial();
-});
+db.sequelize.sync();
 
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
@@ -42,15 +39,5 @@ function initial() {
     Role.create({
       id: 1,
       name: "user"
-    });
-   
-    Role.create({
-      id: 2,
-      name: "moderator"
-    });
-   
-    Role.create({
-      id: 3,
-      name: "admin"
     });
   }
