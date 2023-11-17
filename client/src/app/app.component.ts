8b45f243a448da3374dd04af4fac5243d3b0e4c6
shamito <shamito@google.com>
2023-11-17 15:48:49 -0600

    scrollIntoView made smooth, sections are page height, navbar does not cover

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,21 +3,67 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
+import { Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit { 
   title = 'frontEnd'; 
   message: any; 
-  constructor(private apiService: ApiService) { }; 
+  constructor(
+    private apiService: ApiService,
+    private scroller: ViewportScroller,
+    private router: Router
+    ) { }; 
+
   ngOnInit() { 
+      this.router.navigate(["/"])
       this.apiService.getMessage().subscribe(data => { 
           this.message = data; 
       }); 
   } 
+
+  scrollToTitle(){
+    let title:any = document.getElementById("title")
+    title.
+    scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    })
+  }
+  scrollToAbout(){
+    let title:any = document.getElementById("about")
+    title.
+    scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    })
+  }
+  scrollToProjects(){
+    let title:any = document.getElementById("project")
+    title.
+    scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    })
+  }
+  scrollToContact(){
+    let title:any = document.getElementById("contact")
+    title.
+    scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest"
+    })
+  }
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes, withInMemoryScrolling({anchorScrolling: "enabled"})), provideHttpClient()]
 };
