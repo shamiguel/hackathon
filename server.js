6c643a7b3edc95e8577a5ad5b57feaccd0d7aba6
shamito <shamito@google.com>
2023-11-28 10:56:45 -0600

    change port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 // set port, listen for requests
 
 app.listen(PORT, () => {
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.PORT)
   console.log(`Server is running on port ${PORT}.`);
 });
 
